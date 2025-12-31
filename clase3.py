f0dcbec9605eb570d4add8ee1b45b0adc799dbe7
Erick <eribuit9@gmail.com>
2025-12-30 20:13:20 -0500

    variables y manejo de cadenas

---
+++ clase3.py
@@ -0,0 +1,16 @@
+name = "erick"
+edad = 20
+caracter = 'A'
+desicion = True
+flotante = 3.14
+decimal = 10.5
+
+print(f"Hola, {name}!") #imprime "Hola, erick". la f es para formateo de cadenas
+print(type(name))  #imprime el tipo de variable
+print(type(edad))  #imprime el tipo de variable
+print(type(caracter))  #imprime el tipo de variable 
+print(type(desicion))  #imprime el tipo de variable
+print(type(flotante))  #imprime el tipo de variable
+print(type(decimal))  #imprime el tipo de variable
+
+#type() lo que hace es decirnos que tipo de dato es la variable que le pasamos como argumento
---
+++ clase4.py
@@ -0,0 +1,15 @@
+oracion1 = '''Esto es una oración multilinea.
+podemos escribir variias lineas en una sola variable.
+Incluso con saltos de linea.
+Como este.'''
+
+print(oracion1)
+
+
+oracion2 = """Otra forma de crear oraciones multilinea.
+Esto también es una oración multilinea.
+Podemos escribir varias lineas en una sola variable.
+Incluso con saltos de linea.
+Como este."""
+
+print(oracion2)
