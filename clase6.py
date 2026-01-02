4ea8a47ac8cf0267f0607dcfb6a61191c8e127b0
Erick Buitrago <eribuit9@gmail.com>
2026-01-02 22:25:03 +0000

    clase6: operadores

---
+++ clase6.py
@@ -0,0 +1,37 @@
+#operaciones avanzadas
+a =10
+b = 23
+c = 4.3
+
+suma = a+b
+resta = b - a
+multiplicacion = a * b
+division = b / a
+division_entera = b // a
+modulo = b % a
+potencia = a ** c  #a elevado a c
+print("Operaciones avanzadas con números:")
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
+print(f"División entera: {division_entera}")
+print(f"Módulo: {modulo}")
+print(f"Potencia: {potencia}")
+
+#operador +=
+print("operador '+= ' para incrementar el valor de una variable:")
+x = 10
+print(f"Valor inicial de x: {x}")
+x += 5
+print(f"Valor después de x += 5: {x}")
+#operadores de desigualdad e igualdad
+print("Operadores de desigualdad e igualdad:")
+m = 15
+s = 7
+print(m == s)  # False
+print(m != s)  # True
+print(m > s)   # True
+print(m < s)   # False
+print(m >= s)  # True
+print(m <= s)  # False
