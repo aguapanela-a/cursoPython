d6af59659fa4badecc6ff9713b18d22fa7605bc3
Erick Buitrago <eribuit9@gmail.com>
2026-01-03 20:32:32 +0000

    clase9: copiarlistas con slicing y .copy()

--- clase9.py
+++ clase9.py
@@ -3,3 +3,24 @@ a = [1,2,3,4,5,6]
 b=a
 print(a)
 print(b)
+
+#espacios en memoria:
+print(id(a))
+print(id(b))
+
+##para evitar esto se puede usar slicing  así:
+c = a[:]
+
+print("espacio en memoria de a ",id(a))
+print("espacio en memoria de b ",id(b))
+print("espacio en memoria de c ",id(c))
+
+##t ambién podemos usar el método .copy()
+d = a.copy()
+
+print("espacio en memoria de d ",id(d))
+
+## si agrego algo a "a" de verá reflejado en "b" pero en el resto no
+a.append(345)
+
+print(a,b,c,d)
