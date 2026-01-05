5031fe45be0bddf38620a640ac1ca6903933e730
Erick Buitrago <eribuit9@gmail.com>
2026-01-05 01:27:03 +0000

    clase12: if, elif y else en ppt

---
+++ clase12.py
@@ -0,0 +1,44 @@
+### poiedra papelotijeras con if, elif y else ###
+import random
+jugador1 = input("Ingrese su nombre jugador 1")
+jugador2 = input("Ingrese su nombre jugador 2")
+
+piedra = 1
+papel = 2
+tijera = 3
+
+eleccion_j1 = random.randint(1,3)
+eleccion_j2= random.randint(1,3)
+
+if eleccion_j1 == 1:
+    print(f"{jugador1} eligió piedra")
+    if eleccion_j2 == 1:
+         print(f"{jugador2} eligió piedra \n Hay empate")
+
+    if eleccion_j2 == 2:
+          print(f"{jugador2} eligió papel \n Ganó {jugador2}")
+        
+    if eleccion_j2 == 3:
+         print(f"{jugador2} eligió tijera \n Ganó {jugador1}")
+elif eleccion_j1 == 2:
+    print(f"{jugador1} eligió papel")
+    if eleccion_j2 == 1:
+         print(f"{jugador2} eligió piedra \n Ganó {jugador1}")
+    if eleccion_j2 == 2:
+          print(f"{jugador2} eligió papel \n Hay empate")
+        
+    if eleccion_j2 == 3:
+         print(f"{jugador2} eligió tijera \n Ganó {jugador2}")
+elif eleccion_j1 == 3:
+    print(f"{jugador1} eligió tijera")
+    if eleccion_j2 == 1:
+         print(f"{jugador2} eligió piedra \n Ganó {jugador2}")
+    if eleccion_j2 == 2:
+          print(f"{jugador2} eligió papel \n Ganó {jugador1}")
+        
+    if eleccion_j2 == 3:
+         print(f"{jugador2} eligió tijera \n Hay empate")
+else:
+    print("Error en la elección")
+          
+
