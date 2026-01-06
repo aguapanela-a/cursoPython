82c406b08cb9a144dfe30f4cac4d69fb4438ebe9
Erick Buitrago <eribuit9@gmail.com>
2026-01-06 15:19:04 +0000

    clase16:funciones y parámetro

---
+++ clase16.py
@@ -0,0 +1,8 @@
+## funciones y parámetros en python ##
+## calculadora ##
+def calculadora():
+    while True:
+        operacion = input("Ingrese la operación") ## 34*56+46
+        for char in operacion:
+            if char in ["+","-","*","/"]:
+                pass
