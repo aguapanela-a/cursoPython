b842c34e5b5b0d7da731c0ba1fc50b261bb3ee96
Erick Buitrago <eribuit9@gmail.com>
2026-01-06 02:55:54 +0000

    clase15: Listas por comprension

--- clase15.py
+++ clase15.py
@@ -24,3 +24,20 @@ print(f"Grados celsius: {celcius} \n Grados fahrenheit: {fahrenheit}" )
 
 pares =[x for x in range(0,16,2)]
 print("pares ", pares)
+
+imapres =[x for x in range(1,31) if x%2==1]
+print(f"impares: {imapres}")
+
+## matriz transpuesta ##
+matriz= [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+
+transpose = [[row[i] for row in matriz] for i in range(len(matriz[0]))]
+# 1,4,7
+# 2,5,8
+# 3,6,9
+
+# para i = 0, va a meter en la fila 0 la posición 0 de cada una de las filas de la matriz
+# para i = 1, va a meter en la fila 1 la posición 1 de cada una de las filas de la matriz
+# para i = 2, va a meter en la fila 2 la posición 2 de cada una de las filas de la matriz
