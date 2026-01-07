8d448b06451f4cbc2120c04d23b111e551ab6592
Erick Buitrago <eribuit9@gmail.com>
2026-01-07 17:54:53 +0000

    CLASE16: calculadora,falta poner lnumeros y operadores +- intercalados

--- clase16.py
+++ clase16.py
@@ -11,18 +11,44 @@ def separar_elementos(texto): ## 34*34/4
         
         else:
             operadores.append(char)
-            numero_entero = int(numero_actual)
-            numeros.append(numero_entero)
+            numero_flotante = float(numero_actual)
+            numeros.append(numero_flotante)
             numero_actual = ""
+    
+    numeros.append(float(numero_actual))
     return numeros, operadores
 
-prueba1 = separar_elementos("21*34+999/666")
-print(prueba1)
-### No agarra después del "/" el 666 ###
+
+## ya sepaa bien los números y agarra los operadores
+
+## Aplicar PEMDAS (paréntesis, exponentes,multiplicación, división, adición y substracción)
+#primero * y / lueg + y -
+def calcular(texto):
+    numeros, operadores = separar_elementos(texto)
+    print(f"Números: {numeros}")
+    print(f"Operadores: {operadores}")
+
+    lista_final = []
+    
+    for operador in range(len(operadores)):
+        if operadores[operador] in "*/":        
+            if operadores[operador] == "*":
+                numeros[operador+1] = numeros[operador] * numeros[operador+1]
+                
+            else:  #si es /
+                numeros[operador+1] = numeros[operador] / numeros[operador+1]
+            
+            numeros[operador] = "."
+            operadores[operador] = "."
+    
+    ### FALTA PONER EN UNA LISTA NUMEROS Y OPERADORES INTERCALADOS ##
+        
+    return numeros, operadores
+
+print(calcular("21-34+999/666/75/454*1342+1234"))  ##vamos bien
+
 
             
-        
-
 
 
 
