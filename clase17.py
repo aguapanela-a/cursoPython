d1053e3801c7fda0b8022a7ef1446a43faf79aa9
Erick Buitrago <eribuit9@gmail.com>
2026-01-08 20:05:43 +0000

    clase17: lambda,map

--- clase17.py
+++ clase17.py
@@ -4,3 +4,10 @@ suma = lambda a, b : a + b
 print(suma(1,5))
 
 ## funciones lamba con map para cuando quiero aplicar una función a cada elemento de una lista, así:
+numeros = list(range(11)) ## numeros será una lista con osnúmeros del 0 al 10
+cuadrados = list(map(lambda x: x**2, numeros))  ## la función lambda será aplicada a cada elemento de números (map) de la nueva lista cuadrados (list)
+print(f"Números: {numeros} \nCuadrados: {cuadrados}")
+
+#map(se aplcará esto, a esto)
+
+## ahora usarémos función filter() para filtrar elementos que complen con una condición
