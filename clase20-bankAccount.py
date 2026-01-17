83afd7772300b691d00858ef6070f05a770d3ebf
Erick <eribuit9@gmail.com>
2026-01-16 22:48:59 -0500

    clase20: class Cuenta Bancaria

---
+++ clase20-bankAccount.py
@@ -0,0 +1,53 @@
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder  = account_holder
+        self.balance = balance
+        self.is_Active = True
+    
+    def deposit_withdraw_money(self, amount):
+        if self.is_Active:
+            
+            while True:
+                decision = input(f"¿Qué desea hacer con esos {amount}$?  ----  saldo actual: {self.balance}$ \n  Retirar dinero: A\n  Depositar dinero: B \n -> ")
+                
+                if decision.upper() == "A":
+                    if amount > self.balance:
+                        print("No e puede retirar más de lo que tiene bobo hijueputa")
+                    else:
+                        amount *= -1
+                        break
+                elif decision.upper() == "B":
+                    break
+                    
+                
+            self.balance += amount
+            print(f"La acción seleccionada se realizó con {amount}. El saldo actual es {self.balance}.")
+                
+                
+        else:
+            print("No se puede realiar la acción porque la cuenta está inacitva")
+    
+    def deactivate_Account(self):
+        if self.is_Active:
+            self.is_Active = False
+            print("Cuenta desactivada con éxito")
+        else:
+            print("Su cuenta ya está desactivada bobo hijueputa")
+    
+    def activate_Account(self):
+        if self.is_Active:
+            print("Su cuenta ya está activada bobo hijueputa")
+            
+        else:
+            self.is_Active = True
+            print("Cuenta activada con éxito")
+            
+            
+            
+cuenta1 = BankAccount("Erick", 300)
+
+cuenta1.deposit_withdraw_money(800)
+
+#cuenta1.deactivate_Account()
+
+cuenta1.deposit_withdraw_money(10000)
